0feb76738924a5aa70ce74db78bb8994225a9518
Simon Wheeldon <simon@jampot.design>
2016-10-23 00:17:08 +0100

    back to original

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
                        (process.env.NODE_ENV === 'production' ?
-                            'mongodb://<dbuser>:<dbpassword>@ds063406.mlab.com:63406/heroku_gdw46k4r' :
-                            'mongodb://<dbuser>:<dbpassword>@ds063406.mlab.com:63406/heroku_gdw46k4r');
+                            'mongodb://heroku_gdw46k4r:pevh8mhud5jk9odkq52u4m48u4@ds063406.mlab.com:63406/heroku_gdw46k4r' :
+                            'mongodb://heroku_gdw46k4r:pevh8mhud5jk9odkq52u4m48u4@ds063406.mlab.com:63406/heroku_gdw46k4r');
 exports.PORT = process.env.PORT || 5000;
