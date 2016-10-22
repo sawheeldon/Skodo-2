8b9b9413a85d8a8508896e4a526a9a4e1bb925ef
Simon Wheeldon <simon@jampot.design>
2016-10-22 23:48:28 +0100

    trying to connect to mongoDB

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
                        (process.env.NODE_ENV === 'production' ?
-                            'mongodb://localhost/skodo-2' :
-                            'mongodb://localhost/skodo-2-dev');
+                            'mongodb://<dbuser>:<dbpassword>@ds063406.mlab.com:63406/heroku_gdw46k4r' :
+                            'mongodb://<dbuser>:<dbpassword>@ds063406.mlab.com:63406/heroku_gdw46k4r');
 exports.PORT = process.env.PORT || 5000;
